ba9269a12f4299e345d405ebab2bd25a5e10f549
stephynee <stephykins@gmail.com>
2019-06-11 23:03:28 -0400

    Add remark and source filesystem.

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,5 +1,13 @@
 module.exports = {
   plugins: [
-    `gatsby-plugin-netlify-cms`
+    `gatsby-plugin-netlify-cms`,
+    `gatsby-transformer-remark`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `posts`,
+        path: `${__dirname}/src/cms/blog`
+      }
+    }
   ],
 }
